bc2be061f23d0d1e3a3a1bcac3849ef1211b9e2d
Vikram Narayanan <vikram186@gmail.com>
2023-10-05 20:36:41 -0700

    update script

--- get_cves.sh
+++ get_cves.sh
@@ -2,6 +2,9 @@
 #
 for i in $(seq 2014 2023); do
   TOTAL_CVES=$(wc -l linux-cves-${i}.csv | awk '{print $1}');
-  DRIVER_CVES=$(rg "drivers/.*\.c" linux-cves-${i}.csv | awk '{print $1}' | wc -l);
-  echo "${i} & $((${TOTAL_CVES}-1)) & ${DRIVER_CVES} \\\\\\\\ ";
+  # Checks for drivers/.*\.c
+  #DRIVER_CVES=$(rg "drivers/.*\.c" linux-cves-${i}.csv | awk '{print $1}' | wc -l);
+  # checks for just "driver"
+  DRIVER_CVES=$(rg "driver" linux-cves-${i}.csv | awk '{print $1}' | wc -l);
+  echo "${i} & $((${TOTAL_CVES}-1)) & ${DRIVER_CVES} \\\\ ";
 done
